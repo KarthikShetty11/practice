d9ec4339c65151357d336f4b7457598568b2e493
KarthikShetty11 <kvshetty27@gmail.com>
2022-06-29 15:21:45 +0530

    new update

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-let a = prompt('enter a:');
-let b = prompt('enter b:');
- let temp = a;
- a= b;
- b= temp;
+// let a = prompt('enter a:');
+// let b = prompt('enter b:');
+//  let temp = a;
+//  a= b;
+//  b= temp;
 
- console.log('a:'+a , "b:"+b);
+//  console.log('a:'+a , "b:"+b);
  
+
+
+
+function clicked(){
+    let count=0;
+    count = + 1;
+    document.getElemnentById("click").textContent= "count";
+}
